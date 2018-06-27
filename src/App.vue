93dbd8cd5fd0abbba863c5757b9860985b20d56b
Mark Zuschlag <mzuschlag@MBP-0518.local>
2018-06-27 15:46:48 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <!-- Bind todos data to a prop called 'todos' -->
+    <todo-list v-bind:todos="todos"></todo-list>
+    <!-- When a todo emits 'add-todo', execute the method 'addTodo' -->
+    <create-todo v-on:create-todo="createTodo"></create-todo>
+  </div>
+</template>
+
+<script type="text/javascript">
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  // Building props to send to ToDoList
+  data() {
+    return {
+      /*todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+        id: 0,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+        id: 1,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+        id: 2,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+        id: 3,
+      }],*/
+      todos: [],
+    };
+  },
+  methods: {
+    createTodo(todo) {
+      this.todos.push(todo);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,45 @@
+<!-- Visual part of component -->
+<template>
+  <div>
+    <!-- JS expressions in Vue are enclosed in double curly brackets -->
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <!-- When a todo emits 'complete-todo', execute the method 'completeTodo' -->
+    <!-- When a todo emits 'delete-todo', execute the method 'deleteTodo.' -->
+    <!-- Iterate through todos prop (each item is a 'todo' object from App.vue) -->
+    <!-- When iterating, must give children a unique key (id) -->
+    <!-- Pass the todo object from the todos prop to the prop 'todo' -->
+    <todo v-on:complete-todo="completeTodo" v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:key="todo.id" v-bind:todo="todo"></todo>
+  </div>
+</template>
+
+<!-- Behavior, events, and data storage -->
+<script type="text/javascript" >
+
+import Todo from './Todo';
+
+export default {
+  // ['name of prop being passed', 'name of other prop being passed (?)']
+  props: ['todos'],
+  // Components that this component will render
+  components: {
+    Todo,
+  },
+  // Methods that this component will use
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    completeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+    },
+  },
+};
+
+</script>
+
+<!-- Improve appearance -->
+<style>
+</style>
